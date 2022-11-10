d8d3d49ef8935ffa18edd3af40f53f08a180d3c9
Solaa08 <solarium_08@hotmail.fr>
2022-11-10 19:21:11 +0100

    API data map at homepage

--- frontend/src/pages/Blog.tsx
+++ frontend/src/pages/Blog.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import { postModel } from '../models/postModel';
 
@@ -11,14 +11,29 @@ const Blog: React.FC = () => {
 
 const [data, setData] = useState<any>([])
 
-axios.get('http://127.0.0.1:8000/api/posts/')
-    .then((res) => {
-        console.log(res)
-    })
+const fetchData = () => {
+  axios.get('http://127.0.0.1:8000/api/posts/')
+      .then((res) => {
+          setData(res.data)
+      })}
 
-  return (
-    <div>Blog</div>
-  )
+      useEffect(() => {
+        fetchData()
+      }, [])
+    
+console.log(data)
+    return (
+      <div>
+        {data.map((item:any) =>
+        <div key={item.id}>
+          <h1>{item.title}</h1>
+          <p>{item.content}</p>
+        </div>
+        )}
+      </div>
+    )
+  
+
 }
 
 export default Blog
